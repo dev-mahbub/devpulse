fa1176d0b59c9ed92d7b0dc19cd46619f686efaf
Mahbub Hasan <easycodermahbubwith5500@gmail.com>
2026-05-22 22:33:41 +0600

    create issue done

--- src/modules/issue/issue.controller.ts
+++ src/modules/issue/issue.controller.ts
@@ -1,15 +1,32 @@
 import type { Request, Response } from "express";
 import { issueService } from "./issue.service";
+import sendResponse from "../../utility/sendResponse";
 
 const createIssue = async (req: Request, res: Response) => {
   const token = req.headers.authorization;
+  if (!token) {
+    res.status(500).json({
+      success: false,
+      message: "Unauthorize access!",
+    });
+  }
   try {
     const result = await issueService.createIssueService(
       req.body,
       token as string,
     );
-    console.log("result", result);
-  } catch (error) {}
+    sendResponse(res, {
+      statusCode: 201,
+      success: true,
+      message: "Issue created successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error: error,
+    });
+  }
 };
 
 export const issueController = {
--- src/modules/issue/issue.service.ts
+++ src/modules/issue/issue.service.ts
@@ -1,11 +1,22 @@
+import config from "../../config";
 import { pool } from "../../db";
 import type { IIssue } from "./issue.interface";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 
 const createIssueService = async (payload: IIssue, token: string) => {
-  const user = await pool.query(`SELECT * FROM users `);
-  const result = await pool.query(`
-        INSERT INTO issues(title, description, type, status, reporter_id) VALUES($1, $2, $3, COALESCE($4, status), $5)
-        `);
+  const { title, description, type, status } = payload;
+  const decoded = jwt.verify(token, config.secret as string) as JwtPayload;
+  const { id } = decoded;
+  const user = await pool.query(`SELECT * FROM users WHERE id=$1`, [id]);
+  const userId = user.rows[0].id;
+  const result = await pool.query(
+    `
+        INSERT INTO issues(title, description, type, status, reporter_id)
+         VALUES($1, $2, $3, COALESCE($4, 'open'), $5) RETURNING *
+        `,
+    [title, description, type, status, userId],
+  );
+  return result;
 };
 
 export const issueService = {
